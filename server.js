e5b7db5be164b27b4aad3585468417d38534fa10
Verma92 <verma.rah@husky.neu.edu>
2016-01-21 17:43:34 -0500

    normal changes

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP|| '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+app.get('/hello', function(req, res){
+    res.send('Server.js it is');
+});
 app.listen(port,ipaddress);
