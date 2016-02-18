922dcdc8a79adcdd0439fd42226b3bbfef0ab2ff
Verma92 <verma.rah@husky.neu.edu>
2016-02-18 02:19:03 -0500

    config updated

--- public/Assignments/config.js
+++ public/Assignments/config.js
@@ -8,8 +8,19 @@
             .when("/home",{
                 templateUrl:"views/home/home.view.html"
             })
+            .when("/register",{
+                templateUrl:"views/users/register.view.html"
+            })
             .when("/profile",{
-                templateUrl:"/views/users/profile.view.html"
+                templateUrl:"views/users/profile.view.html"
+            })
+
+            .when("/login",{
+                templateUrl:"views/users/login.view.html"
+            })
+
+            .when("/admin",{
+                templateUrl:"views/admin/admin.view.html"
             })
             .otherwise({
             redirectTo: "/home"
