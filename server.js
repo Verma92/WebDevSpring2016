a798ab0b23810dd228f453e4a52385f0f1b88fdf
RahulVerma <rahulverma@ccs.neu.edu>
2016-03-11 22:03:16 -0500

    assignment2

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 
-var bodyParser = require('body-parser');
+/*
+3000var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+*/
 
 app.get('/hello', function(req, res){
     res.send('hello world');
