57cfce9002b172838ead8f4ef887e35d527636b3
RahulVerma <rahulverma@ccs.neu.edu>
2016-03-18 15:15:15 -0400

    server refactoring

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ var upload = multer(); // for parsing multipart/form-data
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-//require("try.js")();
+
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static(__dirname + '/public'));
+
+require("./try.js")();
 
 
 app.listen(port, ipaddress);
