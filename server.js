ed27705e98e90c89fe36e41eb41b883554b0136f
RahulVerma <rahulverma@ccs.neu.edu>
2016-03-18 14:48:26 -0400

    server setting

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var upload = multer(); // for parsing multipart/form-data
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-require("try.js")();
+//require("try.js")();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static(__dirname + '/public'));
